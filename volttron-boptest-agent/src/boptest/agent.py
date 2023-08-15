042c81a0bd068f5014dac2c33416cf29efbb8354
Kefei Mo <kefei.mo@pnnl.gov>
2023-08-15 15:26:39 -0500

    added peroidic publish

--- volttron-boptest-agent/src/boptest/agent.py
+++ volttron-boptest-agent/src/boptest/agent.py
@@ -1,4 +1,10 @@
 from __future__ import annotations
+
+import datetime
+import time
+from boptest_integration.controllers import *
+import numpy as np
+
 # -*- coding: utf-8 -*- {{{
 # ===----------------------------------------------------------------------===
 #
@@ -43,7 +49,10 @@ from boptest_integration.boptest_integration import BopTestSimIntegrationLocal
 # import numpy as np
 # from boptest_integration.controllers import PidController, SupController, PidTwoZonesController
 from boptest_integration.interface import Interface
+from volttron.utils.scheduling import periodic
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 setup_logging()
 _log = logging.getLogger(__name__)
@@ -96,7 +105,7 @@ class BopTestAgent(Agent):
 
         self._is_onstart_completed = False
 
-
+        self.poll_freq = 3  # the frequency to poll simulation result
 
     # @staticmethod
     # def boptest_up(testcase: str, docker_compose_file_path: str, is_verbose: bool = True) -> str:
@@ -121,12 +130,6 @@ class BopTestAgent(Agent):
         Called after any configurations methods that are called at startup.
         Usually not needed if using the configuration store.
         """
-        pass
-        interface = Interface(config=self.config)
-        kpi, res, forecasts, custom_kpi_result = interface.run_workflow()
-        print(f"======= kpi {kpi}")
-
-        logging.info("=========== refactoring onstart")
 
         # # GET TEST INFORMATION
         # # -------------------------------------------------------------------------
@@ -283,29 +286,17 @@ class BopTestAgent(Agent):
 
         # VIEW RESULTS
         # -------------------------------------------------------------------------
-        # Report KPIs
-        kpi = self.bp_sim.get_kpi()
-        for key in kpi.keys():
 
-            # return kpi, res, custom_kpi_result, forecasts  # Note: originally publish these
-            # TODO: refactor topic value to config
-            default_prefix = "PNNL/BUILDING/UNIT/"
-            self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "kpi", message=str(kpi))
-            # self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "result", message=str(res))
-            # TODO: publish custom_kpi_result forecasts
-
-            # Store the result data
-            self._results = res
-            self._kpi = kpi
-            # self._custom_kpi_result = custom_kpi_result
-            self._forecasts = forecasts
-
-            self._is_onstart_completed = True
+        if False:  # if not using cosimulation
+            _ = self._publish_final_output()
+            logging.info(f"======== _ {_} ======")
+        else:
+            # periodic testing
+            self.rt_periodic = self.core.periodic(self.poll_freq,
+                                                  self._run_periodic,
+                                                  wait=0)
 
         logging.info("======== onstart completed.======")
-
-
-
 
         # Example publish to pubsub
         # self.vip.pubsub.publish('pubsub', "some/random/topic", message="HI!")
@@ -314,6 +305,43 @@ class BopTestAgent(Agent):
         # # self.vip.rpc.call("some_agent", "some_method", arg1, arg2)
         # pass
         # self._create_subscriptions(self.setting2)
+
+    def _publish_final_output(self):
+        """
+        retrieve and publish the final (i.e., end of the simulation) output (results/measurements, kpis, predicts.)
+        """
+        interface = Interface(config=self.config)
+        kpi, res, forecasts, custom_kpi_result = interface.run_workflow()
+        print(f"======= kpi {kpi}")
+
+        # Report KPIs
+        kpi = self.bp_sim.get_kpi()
+        for key in kpi.keys():
+            # return kpi, res, custom_kpi_result, forecasts  # Note: originally publish these
+            # TODO: refactor topic value to config
+            default_prefix = "PNNL/BUILDING/UNIT/"
+            self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "kpi", message=str(kpi))
+            # self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "result", message=str(res))
+            # TODO: publish custom_kpi_result forecasts
+
+        # Store the result data
+        self._results = res
+        self._kpi = kpi
+        # self._custom_kpi_result = custom_kpi_result
+        self._forecasts = forecasts
+
+        self._is_onstart_completed = True
+        return res, kpi, forecasts
+
+    # @Core.schedule(periodic(5))
+    def _run_periodic(self):
+        """Periodic call
+
+        Used to maintain the time since each topic's last publish.
+        Sends an alert if any topics are missing.
+        """
+        logging.info((f"************** {datetime.datetime.now()} ************** "))
+        self.run_workflow_2()
 
     def _parse_config(self, config_path: str) -> Dict:
         """Parses the agent's configuration file.
@@ -357,6 +385,192 @@ class BopTestAgent(Agent):
         else:
             logging.info("Onstart process not finished")
             return None
+
+    def run_workflow_2(self):
+        """
+        run workflow
+        """
+        pass
+        logging.info("=========== run_workflow")
+
+        # GET TEST INFORMATION
+        # -------------------------------------------------------------------------
+        logger.info('\nTEST CASE INFORMATION\n---------------------')
+        # Retrieve testcase name from REST API
+        name = self.bp_sim.get_name()
+        logger.info('Name:\t\t\t\t{0}'.format(name))
+        # check if running the proper testcase
+        testcase_name = self.config.get("testcase_name")
+        if name != testcase_name:
+            raise ValueError(fr"Running testcase `{name}` inconsistent with config testcase_name `{testcase_name}`.")
+        # Retrieve a list of inputs (controllable points) for the model from REST API
+        inputs = self.bp_sim.get_inputs(keys_only=False)
+        logger.info('Control Inputs:\t\t\t{0}'.format(inputs))
+        # Retrieve a list of measurements (outputs) for the model from REST API
+        measurements = self.bp_sim.get_measurements(keys_only=False)
+        logger.info('Measurements:\t\t\t{0}'.format(measurements))
+        # Get the default simulation timestep for the model for simulation run
+        step_def = self.bp_sim.get_step()
+        logger.info('Default Control Step:\t{0}'.format(step_def))
+
+        # # IF ANY CUSTOM KPI CALCULATION, DEFINE STRUCTURES
+        # # ------------------------------------------------
+        # custom_kpis = []  # Initialize customized kpi calculation list
+        # custom_kpi_result = {}  # Initialize tracking of customized kpi calculation results
+        # if customized_kpi_config is not None:
+        #     with open(customized_kpi_config) as f:
+        #         config = json.load(f, object_pairs_hook=collections.OrderedDict)
+        #     for key in config.keys():
+        #         custom_kpis.append(CustomKPI(config[key]))
+        #         custom_kpi_result[CustomKPI(config[key]).name] = []
+        # custom_kpi_result['time'] = []
+
+        # RUN TEST CASE
+        # -------------------------------------------------------------------------
+        # Record real starting time
+        start = time.time()
+        # Initialize test case
+        logger.info('Initializing test case simulation.')
+        # TODO: investigate what happen if length is not a multiplication of steps (affected by PUT/results)
+        # TODO: handle very large length, e.g., 365 * 24 * 3600
+        length = self.config.get("length")  # intermediate variable to calculate total_time_steps.
+
+        step = self.config.get("step")  # step length
+        scenario = self.config.get("scenario")  # e.g., {"time_period": "test_day","electricity_price": "dynamic"}
+        if scenario is not None:
+            # Initialize test with a scenario time period
+            res = self.bp_sim.put_scenario(**scenario)
+            logging.info('Scenario:\t\t\t{0}'.format(res))
+            # Record test simulation start time
+            start_time = int(res["time_period"]['time'])  # Note: the schema can be subjective to Boptest versions.
+            # Set final time and total time steps to be very large since scenario defines length
+            final_time = np.inf
+            total_time_steps = int(length / step)
+        else:
+            # Initialize test with a specified start time and warmup period
+            start_time = self.config.get("initialize").get("start_time")  # used in GET/initialize
+            warmup_period = self.config.get("initialize").get("warmup_period")  # used in GET/initialize
+
+            res = self.bp_sim.put_initialize(start_time=start_time, warmup_period=warmup_period)
+            logging.info("RESULT: {}".format(res))
+            # Set final time and total time steps according to specified length (seconds)
+            final_time = start_time + length
+            total_time_steps = int(length / step)  # calculate number of timesteps, i.e., number of advance
+        if res:
+            logging.info('Successfully initialized the simulation')
+        logger.info('\nRunning test case...')
+        # Set simulation time step
+        res = self.bp_sim.put_step(step=step)
+
+        # Load controller info
+        # type (currently support pid, sup, pidTwoZones)
+        controller_type = self.config.get("controller").get("type")
+        # Initialize input to simulation from controller
+        u = self.config.get("controller").get("u")
+        # Initialize forecast storage structure
+        forecast_data = self.config.get("controller").get("forecast_parameters")
+
+        if controller_type == "pid":
+            controller = PidController(u=u)
+        elif controller_type == "sup":
+            controller = SupController(u=u)
+        elif controller_type == "pidTwoZones":
+            controller = PidTwoZonesController(u=u, forecast_parameters=forecast_data)
+        else:
+            error_msg = "controller type needs to be one of ['pid', 'sup', 'pidTwoZones']"
+            logging.error(error_msg)
+            raise ValueError(error_msg)
+
+        # init using scenario endpoint
+        # res = self.bp_sim.get_scenario()
+        # logging.info("RESULT: {}".format(res))
+
+        # Simulation Loop
+        custom_kpi_result = None
+        forecasts = None
+        # Advance simulation with control input value(s)
+        y = self.bp_sim.post_advance(data=u)
+        # If simulation is complete break simulation loop
+        if not y:
+            return
+        # If custom KPIs are configured, compute the KPIs
+        # TODO: develop customer-kpis feature
+        # for kpi in custom_kpis:
+        #     kpi.processing_data(y)  # Process data as needed for custom KPI
+        #     custom_kpi_value = kpi.calculation()  # Calculate custom KPI value
+        #     custom_kpi_result[kpi.name].append(round(custom_kpi_value, 2))  # Track custom KPI value
+        #     print('KPI:\t{0}:\t{1}'.format(kpi.name, round(custom_kpi_value, 2)))  # Print custom KPI value
+        # custom_kpi_result['time'].append(y['time'])  # Track custom KPI calculation time
+        # If controller needs a forecast, get the forecast data and provide the forecast to the controller
+        if controller.use_forecast:
+            # Retrieve forecast from restful API
+            forecast_parameters = controller.get_forecast_parameters()
+            # forecast_data = check_response(requests.put('{0}/forecast'.format(url), json=forecast_parameters))
+            forecast_data = self.bp_sim.put_forecast(**forecast_parameters)
+            # Use forecast data to update controller-specific forecast data
+            forecasts = controller.update_forecasts(forecast_data=forecast_data, forecasts=forecasts)
+        else:
+            forecasts = None
+        # Compute control signal input to simulation for the next timestep
+        u = controller.compute_control(y, forecasts)
+        logger.info('\nTest case complete.')
+        logger.info('Elapsed time of test was {0} seconds.'.format(time.time() - start))
+
+        # VIEW RESULTS
+        # -------------------------------------------------------------------------
+        # Report KPIs
+        kpi = self.bp_sim.get_kpi()
+        logger.info('\nKPI RESULTS \n-----------')
+        for key in kpi.keys():
+            if key == 'ener_tot':
+                unit = 'kWh/m$^2$'
+            elif key == 'pele_tot':
+                unit = 'kW/m$^2$'
+            elif key == 'pgas_tot':
+                unit = 'kW/m$^2$'
+            elif key == 'pdih_tot':
+                unit = 'kW/m$^2$'
+            elif key == 'tdis_tot':
+                unit = 'Kh/zone'
+            elif key == 'idis_tot':
+                unit = 'ppmh/zone'
+            elif key == 'cost_tot':
+                unit = 'Euro or \$/m$^2$'
+            elif key == 'emis_tot':
+                unit = 'KgCO2/m$^2$'
+            elif key == 'time_rat':
+                unit = 's/s'
+            else:
+                unit = None
+            logging.info('{0}: {1} {2}'.format(key, kpi[key], unit))
+
+            # POST PROCESS RESULTS
+            # -------------------------------------------------------------------------
+            # Get result data
+            points = list(measurements.keys()) + list(inputs.keys())
+            res = self.bp_sim.put_results(point_names=points, start_time=start_time, final_time=final_time)
+
+            # # Store the result data
+            # self._results = res
+            # self._kpi = kpi
+            # # self._custom_kpi_result = custom_kpi_result
+            # self._forecasts = forecasts
+            #
+            # self._is_onstart_completed = True
+
+        logger.info("======== run workflow_2 completed.======")
+
+        # Report KPIs
+        kpi = self.bp_sim.get_kpi()
+        for key in kpi.keys():
+            # return kpi, res, custom_kpi_result, forecasts  # Note: originally publish these
+            # TODO: refactor topic value to config
+            default_prefix = "PNNL/BUILDING/UNIT/"
+            self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "kpi", message=str(kpi))
+            # self.vip.pubsub.publish(peer='pubsub', topic=default_prefix + "result", message=str(res))
+            # TODO: publish custom_kpi_result forecasts
+
+        return kpi, res, forecasts, custom_kpi_result  # Note: originally publish these
 
 
 def main():
